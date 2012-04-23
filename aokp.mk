e82ca93a6268c865ab852c0ac6a3a5b17831faee
Hashcode <hashcode0f@gmail.com>
2012-04-22 23:51:18 -0700

    fix device naming
    Change-Id: I8a7ec1f97fa3781a99cc4f65f159f111a16718e9

--- aokp.mk
+++ aokp.mk
@@ -11,6 +11,6 @@ $(call inherit-product, device/amazon/otter/full_otter.mk)
 PRODUCT_NAME := aokp_otter
 PRODUCT_BRAND := android
 PRODUCT_DEVICE := otter
-PRODUCT_MODEL := Amazon Kindle Fire
-PRODUCT_MANUFACTURER := amazon
+PRODUCT_MODEL := Kindle Fire
+PRODUCT_MANUFACTURER := Amazon
 
